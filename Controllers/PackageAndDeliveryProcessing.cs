4712fac13fc68287990828515d6cb311ef55758f
vishal1221-1221 <65186356+vishal1221-1221@users.noreply.github.com>
2022-08-20 13:06:46 +0530

    welcome

---
+++ Controllers/PackageAndDeliveryProcessing.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using ChargeProceesing.API.Models;
+using ChargeProceesing.API.Models.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChargeProceesing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackageAndDeliveryProcessing : ControllerBase
+    {
+        //------------------------------------------------------------------------
+        [HttpGet]
+        public IActionResult GetPackProcessing([FromQuery] GetDto data)
+        {
+
+            if (data.comType == "Integral")
+            {
+                data.packageCharges = 100 * data.quantity;
+                data.deliveryCharges = 200 * data.quantity;
+            }
+            else
+            {
+                data.packageCharges = 50 * data.quantity;
+                data.deliveryCharges = 100 * data.quantity;
+            }
+            return Ok(data);
+        }
+        //--------------------------------------------------------------------------------------
+    }
+}
---
+++ Models/Dtos/GetDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChargeProceesing.API.Models.Dtos
+{
+    public class GetDto
+    {
+        public string comType { get; set; }
+        public int quantity { get; set; }
+        public long packageCharges { get; set; }
+        public long deliveryCharges { get; set; }
+
+    }
+}
